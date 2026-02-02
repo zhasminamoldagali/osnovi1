ca69272983f92944e6776a10a5db4f65ac8ef1dd
zhasmina <moldazhasmi@gmail.com>
2026-02-02 09:15:20 +0500

    задание18

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-score = int(input("Введите балл (1-5): "))
-if score == 5:
-    print("Отлично")
-elif score == 4:
-    print("Хорошо")
-else:
-    print("Старайся")
+for i in range(5, 0, -1):
+    print(i)
