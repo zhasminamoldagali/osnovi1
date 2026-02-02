475ce1416bdc629c38dd71fe94ee028c7572669b
zhasmina <moldazhasmi@gmail.com>
2026-02-02 19:16:40 +0500

    задание32

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-for i in range(1, 5):
-    print(i**2, end=' ')
-print()
+t = int(input("Введи температуру: "))
+if t < 0:
+    print("Мороз")
+elif t > 25:
+    print("Жара")
+else:
+    print("Нормально")
