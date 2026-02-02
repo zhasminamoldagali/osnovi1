f826de422c95ab76f1aa553c2d5a7cc626a1895d
zhasmina <moldazhasmi@gmail.com>
2026-02-02 19:15:49 +0500

    задание30

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-a = int(input("Введите первое число:"))
-b = int(input("Введите второе число:"))
-print((a + b)/2)
+login = input("Введите логин: ")
+if login != "admin":
+    print("Доступ запрещен")
+else:
+    print("Доступ разрешен")
