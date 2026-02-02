4757a83b20a82ff76bce356fafb089ac9000ead0
zhasmina <moldazhasmi@gmail.com>
2026-02-02 19:14:53 +0500

    задание28

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-a = int(input("Введите первое число:"))
-b = int(input("Введите второе число:"))
-if a > 0 and b > 0:
-    print(a*b)
+n = int(input("Введите число n:"))
+while n >=0:
+    print(n, end=' ')
+    n -= 1
+print()
